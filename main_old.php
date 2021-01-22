23eea86a036349560da08386ca6b0a1e6cff1b46
DaisukeDaisuke <akblcmakblcm@gmail.com>
2021-01-22 17:53:20 +0900

    Add files via upload

--- main_old.php
+++ main_old.php
@@ -2,19 +2,31 @@
 include __DIR__."/vendor/autoload.php";
 
 use PhpParser\Node\Expr;
-use PhpParser\Node\Expr\AssignOp\Mul;
 use PhpParser\Node\Expr\BinaryOp;
+use PhpParser\Node\Expr\BinaryOp\BitwiseAnd;
+use PhpParser\Node\Expr\BinaryOp\BitwiseOr;
+use PhpParser\Node\Expr\BinaryOp\BooleanAnd;
+use PhpParser\Node\Expr\BinaryOp\BooleanOr;
+use PhpParser\Node\Expr\BinaryOp\Coalesce;
+use PhpParser\Node\Expr\BinaryOp\Concat;
+use PhpParser\Node\Expr\BinaryOp\Div;
+use PhpParser\Node\Expr\BinaryOp\Minus;
+use PhpParser\Node\Expr\BinaryOp\Mul;
 use PhpParser\Node\Expr\BinaryOp\Plus;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Scalar;
 use PhpParser\Node\Scalar\LNumber;
+use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\Echo_;
 use PhpParser\NodeDumper;
 use PhpParser\ParserFactory;
+use pocketmine\utils\Binary;
 
 $code = '
 <?php
 $a = 100;
-echo 1+2*(3/$a);
+//echo 1+2*(3/$a*1);
+echo ((2*1+1)+(2/1+3)-(2/(5*6+20)*(5*(6/2)))) & 1;//3+5+50
 ';
 
 //$code = file_get_contents("/sdcard/www/public/php-parser/vendor/unphar.php");
@@ -23,50 +35,264 @@ $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
 $stmts = $parser->parse($code);
 //$oldTokens = $parser->getTokens();
 //var_dump($stmts);
-$dumper = new NodeDumper([
-	'dumpComments' => true,
-]);
+
+$dumper = new NodeDumper(['dumpComments' => true,]);
 echo $dumper->dump($stmts, $code);
 
-var_dump($stmts);
+
+//var_dump($stmts);
 
 class code{
-	const ADD = 0;//add 80 1000 $a
-	const readv = 1;//readv $a(0)
+
+	const READV = "\x00";//readv $a(0)
+	const INT = "\x01";
+	const ADD = "\x02";//add 80 1000 $a
+	const MUL = "\x03";
+	const DIV = "\x04";
+	const MINUS = "\x05";
+	const B_AND = "\x06";
+	const B_OR = "\x07";
+	const B_XOR = "\x08";
+	const BOOL_AND = "\x09";
+	const BOOL_OR = "\x0A";
+	const COALESCE = "\x0B":
+	const CONCAT = "\x0C"
+
+
+	const PRINT = "\xff";
 
 }
 
 class main_old{
+	const TYPE_BYTE = 0;
+	const TYPE_SHORT = 1;
+	const TYPE_INT = 2;
+	const TYPE_LONG = 3;
+
+	public $count = 0;
+
+
 	/** @var int */
 	public $blockid = 1;
 	/** @var CodeBlock[] */
 	public $block = [];
 
-	public function __construct($block){
+	public function __construct(){
 		$this->block[$this->blockid] = new CodeBlock($this->blockid);
+		var_dump($this->checkIntSize(254));
 	}
 
-	function getcode($node){
+	function execStmt($node){
+		var_dump("execStmt");
+		switch(get_class($node)){
+			case Echo_::class:
+				var_dump("echo");
+				return code::PRINT.$this->execStmts($node->exprs);
+				break;
+		}
+	}
+
+	function execStmts(array $nodes){
+		$return = "";
+		foreach($nodes as $node){
+			if($node instanceof Expr){
+				$return .= $this->execExpr($node);
+			}
+			if($node instanceof Stmt){
+				$return .= $this->execStmt($node);
+			}
+			/*if($node instanceof node){
+
+			}*/
+		}
+		return $return;
+	}
+
+	function execExpr(Expr $expr){
+		if($expr instanceof BinaryOp){
+			$return = $this->execBinaryOp($expr);
+			var_dump($return);
+			$values = [];
+			foreach($return as $value){
+				var_dump($value);
+
+				$var = $value[0];
+				$var1 = $this->test($value[1], $values);
+				$var2 = $this->test($value[2], $values);
+				$output = $value[3];
+
+
+				$return1 = 0;
+				switch($var){
+					case code::ADD:
+						$return1 = $var1 + $var2;
+						break;
+					case code::MUL:
+						$return1 = $var1 * $var2;
+						break;
+					case code::DIV:
+						$return1 = $var1 / $var2;
+						break;
+					case code::MINUS:
+						$return1 = $var1 - $var2;
+						break;
+					case code::B_AND:
+						$return1 = $var1 & $var2;
+						break;
+					case code::B_OR:
+						$return1 = $var1 | $var2;
+						break;
+					case code::B_XOR:
+						$return1 = $var1 ^ $var2;
+						break;
+					case code::BOOL_AND:
+						$return1 = (int) $var1 && $var2;
+						break;
+					case code::BOOL_OR:
+						$return1 = (int) $var1 || $var2;
+						break;
+					case code::COALESCE:
+						//$return1 = $var1 ?? $var2;
+						break;
+					case code::CONCAT:
+						$return1 = (int) $var1 . $var2;
+						break;
+					case code::BOOL_OR:
+						$return1 = (int) $var1 || $var2;
+						break;
+					case code::BOOL_OR:
+						$return1 = (int) $var1 || $var2;
+						break;
+					case code::BOOL_OR:
+						$return1 = (int) $var1 || $var2;
+						break;
+					case code::BOOL_OR:
+						$return1 = (int) $var1 || $var2;
+						break;
+					case code::BOOL_OR:
+						$return1 = (int) $var1 || $var2;
+						break;
+					case code::BOOL_OR:
+						$return1 = (int) $var1 || $var2;
+						break;
+					case code::BOOL_OR:
+						$return1 = (int) $var1 || $var2;
+						break;
+					case code::BOOL_OR:
+						$return1 = (int) $var1 || $var2;
+						break;
+				}
+				var_dump($output." => ".$return1);
+				$values[$output] = $return1;
+				//var_dump($values);
+			}
+			var_dump($return1);
+			//return var_dump($this->execBinaryOp($expr));
+		}
+	}
+
+	function test($value, &$array){
+		if(strpos($value, 'H') !== false){
+			return (int) substr($value, 0, -1);
+		}
+		//var_dump($value);
+		$return = $array[$value];
+		unset($array[$value]);
+		return $return;
+	}
+
+	function execBinaryOp($node, $count = 0){//output //add 10 10 1
+		//var_dump($count);
 		switch(get_class($node)){
 			case Plus::class:
-				$this->execScalar_var($node->right);//opcode
-				break;
+				return $this->execbinaryplus($node, code::ADD, $count);
 			case Mul::class:
-				$node->left;
-				$node->right;
-				break;
+				return $this->execbinaryplus($node, code::MUL, $count);
+			case Div::class:
+				return $this->execbinaryplus($node, code::DIV, $count);
+			case Minus::class:
+				return $this->execbinaryplus($node, code::MINUS, $count);
+			case BitwiseAnd::class:
+				return $this->execbinaryplus($node, code::B_AND, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+
+
+				case BooleanAnd::class:
+				return $this->execbinaryplus($node, code::BOOL_AND, $count);
+			case BooleanOr::class:
+				return $this->execbinaryplus($node, code::BOOL_OR, $count);
+			case Coalesce::class:
+				return $this->execbinaryplus($node, code::COALESCE, $count);
+			case Concat::class:
+				return $this->execbinaryplus($node, code::CONCAT, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
+			case BitwiseOr::class:
+				return $this->execbinaryplus($node, code::B_OR, $count);
 
 		}
 
 	}
 
-	function execScalar_var($value){//array...?
+	function execbinaryplus($node, $id, $count = 0){
+		$left = $this->execScalar_var($node->left, $count + 1);
+		$right = $this->execScalar_var($node->right, $count + 2);
+
+		$return = [];
+		if(is_array($left)&&is_array($right)){
+			$return = [...$left, ...$right];
+			$return[] = [$id, $count + 1, $count + 2, $count];//$left,$right
+		}else if(is_array($left)){
+			$return = $left;
+			$return[] = [$id, $count + 1, $right, $count];
+		}else if(is_array($right)){
+			$return = $right;
+			$return[] = [$id, $left, $count + 2, $count];
+		}else{
+			//$count++;
+			$return[] = [$id, $left, $right, $count];
+			//$count-=2;
+
+		}
+		//var_dump($return);
+		return $return;
+	}
+
+	function execScalar_var($value, $count){//array...?
 		if($value instanceof Scalar){
 			return $this->execScalar($value);
 		}
 
 		if($value instanceof BinaryOp){
-			return $this->execBinaryOp($value);
+			return $this->execBinaryOp($value, $count);
+			//return $this->execBinaryOp($value);
 		}
 
 		if($value instanceof Variable){
@@ -79,7 +305,7 @@ class main_old{
 		if($node->name instanceof Expr){
 			return "";//$$b
 		}
-		return chr(code::readv).$this->getValualueId($node->name);
+		return code::READV.$this->getValualueId($node->name);
 	}
 
 	public function getValualueId($value){
@@ -87,48 +313,56 @@ class main_old{
 	}
 
 
-	function execBinaryOp($node){
+	/*function execBinaryOp($node){
 		switch(get_class($node)){
 			case Plus::class:
 				return "";
 				break;
 		}
-	}
+	}*/
 
 	function execScalar($node){
 		switch(get_class($node)){
 			case LNumber::class:
-				$value = $node->value;
-				$intsize = $this->checkIntSize($value);
+				//$value = $node->value;
+				//$intsize = $this->checkIntSize($value);
 				//return [$intsize, $value];
-				return $value;
+				return $this->getInt($node->value);
 				break;
 		}
 	}
 
+	function getInt($value){
+		return $value."H";
+		$size = $this->checkIntSize($value);
+		$return = code::INT.chr($size);
+		switch($size){
+			case self::TYPE_BYTE://byte
+				$return .= Binary::readSignedByte($value);
+				break;
+			case self::TYPE_SHORT://short
+				$return .= Binary::writeLShort($value);
+				break;
+			case self::TYPE_INT://int
+				$return .= Binary::writeInt($value);
+				break;
+			case self::TYPE_LONG://long
+				$return .= Binary::writeLong($value);
+				break;
+		}
+		return $return;
+	}
+
 	function checkIntSize($value){
-		if($value < 0){
-			switch(true){
-				case $value >= -0xff://byte
-					return 5;
-				case $value >= -0xffff://short
-					return 6;
-				case $value >= -0x7FFFFFFF://int
-					return 7;
-				case $value >= -0x7FFFFFFFFFFFFFFF://long
-					return 8;
-			}
-		}else{
-			switch(true){
-				case $value <= 0xff://byte
-					return 1;
-				case $value <= 0xffff://short
-					return 2;
-				case $value <= 0x7FFFFFFF://int
-					return 3;
-				case $value <= 0x7FFFFFFFFFFFFFFF://long
-					return 4;
-			}
+		switch(true){
+			case $value <= 127&&$value >= -128://byte
+				return self::TYPE_BYTE;
+			case $value <= 0xffff&&$value >= -0xffff://short
+				return self::TYPE_SHORT;
+			case $value <= 0x7FFFFFFF&&$value >= -0x7FFFFFFF://int
+				return self::TYPE_INT;
+			case $value <= 0x7FFFFFFFFFFFFFFF&&$value >= -0x7FFFFFFFFFFFFFFF://long
+				return self::TYPE_LONG;
 		}
 	}
 }
@@ -150,7 +384,7 @@ class CodeBlock{
 	}
 
 	function get($value){
-		return $this->ids[$value];
+		return $this->ids[$value] ?? 1;//debug
 	}
 
 	function add($value){//変数名
@@ -163,6 +397,11 @@ class CodeBlock{
 	}
 
 }
+
+$main_old = new main_old();
+$output = $main_old->execStmts($stmts);
+var_dump($output);
+file_put_contents("output.txt", $output);
 //var_dump(token_get_all($code));
 
 
